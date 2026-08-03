680379c91cb61d2a11a7b560ca56a7dcd733dba8
sunnydays0424 <sunheekim1998@gmail.com>
2026-08-03 22:39:44 +0900

    수정 완:

--- src/component/invitation/index.tsx
+++ src/component/invitation/index.tsx
@@ -33,17 +33,13 @@ export const Invitation = () => {
         <div className="break" />
 
         {/* 초대 문구 */}
-        <div className="content">싱그러운 여름 향기 가득한 날</div>
-        <div className="content">소중한 분들을 모시고</div>
-        <div className="content">사랑의 약속을 하려고 합니다.</div>
+        <div className="content">삶을 바라보는 시선이 닮은</div>
+        <div className="content">저희 두 사람이 이제</div>
+        <div className="content">같은 계절 안에서 함께 살아가려 합니다.</div>
         <div className="break" />
-        <div className="content">햇살이 뜨거울 땐 가려주고,</div>
-        <div className="content">비가 오면 우산이 되어주는</div>
-        <div className="content">부부가 되겠습니다.</div>
-        <div className="break" />
-        <div className="content">기쁜날 함께 하시어</div>
-        <div className="content">저희의 앞날을 축복해 주세요.</div>
-
+        <div className="content">따뜻한 마음으로</div>
+        <div className="content">저희의 첫 걸음을 축복해주시면</div>
+        <div className="content">감사하겠습니다.</div>
         <div className="break" />
 
         {/* 혼주 및 신랑 정보 */}
