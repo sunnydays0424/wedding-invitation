091116730f61dc05a1c4a7f2f781955c5c2f1ffa
sunnydays0424 <sunheekim1998@gmail.com>
2026-08-03 22:29:28 +0900

    모바일 청첩장 수정중

--- src/const.ts
+++ src/const.ts
@@ -37,7 +37,7 @@ export const LOCATION = "상록아트홀 5층 아트홀"
 /**
  * 예식 장소 상세 주소
  */
-export const LOCATION_ADDRESS = "서울시 관악구 관악로 1, 연구공원 본관 1층"
+export const LOCATION_ADDRESS = "서울특별시 강남구 언주로 508 (역삼동 679-1) 상록회관 5층 아트홀"
 
 /**
  * 카카오톡 공유 시 사용할 위치 정보 주소
@@ -70,9 +70,9 @@ export const KMAP_PLACE_ID = 8634826
 // 신부 정보 설정
 export const BRIDE_FULLNAME = "김선희"
 export const BRIDE_FIRSTNAME = "선희"
-export const BRIDE_TITLE = "장녀"
-export const BRIDE_FATHER = "정상원"
-export const BRIDE_MOTHER = "박윤정"
+export const BRIDE_TITLE = "딸"
+export const BRIDE_FATHER = "김종희"
+export const BRIDE_MOTHER = "유서진"
 
 /**
  * 신부측 연락처 및 계좌 정보
@@ -99,11 +99,11 @@ export const BRIDE_INFO = [
 ]
 
 // 신랑 정보 설정
-export const GROOM_FULLNAME = "남주호"
-export const GROOM_FIRSTNAME = "주호"
-export const GROOM_TITLE = "차남"
-export const GROOM_FATHER = "남현태"
-export const GROOM_MOTHER = "김현영"
+export const GROOM_FULLNAME = "김동제"
+export const GROOM_FIRSTNAME = "동제"
+export const GROOM_TITLE = "아들"
+export const GROOM_FATHER = "김창수"
+export const GROOM_MOTHER = "강영숙"
 
 /**
  * 신랑측 연락처 및 계좌 정보
