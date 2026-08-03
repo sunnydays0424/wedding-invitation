089cb50ec20a47e3c0b9a6f1c1f06c617d2bbab7
sunnydays0424 <sunheekim1998@gmail.com>
2026-08-03 22:51:05 +0900

    제발

--- src/const.ts
+++ src/const.ts
@@ -53,7 +53,7 @@ export const SHARE_ADDRESS_TITLE = LOCATION
 /**
  * 지도 서비스(네이버, 카카오)에 사용할 좌표 [경도, 위도]
  */
-export const WEDDING_HALL_POSITION = [37.5038561, 127.0428303]
+export const WEDDING_HALL_POSITION = [127.0428303, 37.5038561]
 
 /**
  * 네이버 지도 장소 ID (NMAP_PLACE_ID)
