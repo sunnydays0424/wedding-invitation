11d9867f1c3e18a62b7c70a439a97083b5dc25df
sunnydays0424 <sunheekim1998@gmail.com>
2026-08-04 21:55:50 +0900

    수정아트홀

--- src/const.ts
+++ src/const.ts
@@ -32,7 +32,7 @@ export const HOLIDAYS = []
 /**
  * 예식 장소 명칭
  */
-export const LOCATION = "상록아트홀"
+export const LOCATION = "상록아트홀 5층 아트홀"
 
 /**
  * 예식 장소 상세 주소
@@ -43,7 +43,7 @@ export const LOCATION_ADDRESS = "서울특별시 강남구 언주로 508 (역삼
  * 카카오톡 공유 시 사용할 위치 정보 주소
  * 필요에 따라 LOCATION과 다르게 설정할 수 있습니다.
  */
-export const SHARE_ADDRESS = LOCATION
+export const SHARE_ADDRESS = "상록아트홀"
 
 /**
  * 카카오톡 공유 시 표시될 위치 제목
