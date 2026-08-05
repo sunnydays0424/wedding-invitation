b07fadc6ecd03be907f5bc4becc1c2b78a4bb0c8
sunnydays <70943816+sunnydays0424@users.noreply.github.com>
2026-08-05 14:05:51 +0900

    계좌멘트수정

--- src/component/information/index.tsx
+++ src/component/information/index.tsx
@@ -38,9 +38,9 @@ export const Information2 = () => {
       <div className="info-card">
         <div className="label">마음 전하기</div>
         <div className="content">
-          참석이 어려워 직접 축하해주지 못하는
+          직접 축하를 건네지 못하는 분들을 위해
           <br />
-          분들을 위해 계좌번호를 기재하였습니다.
+          마음 전하실 곳을 함께 적어둡니다.
           <br />
           넓은 마음으로 양해 부탁드립니다.
         </div>
