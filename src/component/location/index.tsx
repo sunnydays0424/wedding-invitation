fd439537581e6384e71e0bdebf3f5d8fcf619455
sunnydays <70943816+sunnydays0424@users.noreply.github.com>
2026-08-05 13:51:42 +0900

    span으로

--- src/component/location/index.tsx
+++ src/component/location/index.tsx
@@ -43,7 +43,7 @@ export const Location = () => {
             <br />
             ① <b>KT 강남지사</b> 하차
             <br />
-            → <div style={{ color: "blue" }}>간선(파랑)</div>: 141(도봉산), 242(중랑, 신내역),
+            → <span style={{ color: "blue" }}>간선(파랑)</span>: 141(도봉산), 242(중랑, 신내역),
             <br />
             &nbsp;&nbsp;&nbsp;&nbsp;361(여의도)
             <br />
